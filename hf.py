d2b2664332ceca77ff990ed9d619b0a8ae3abbb6
Vagrant Default User <vagrant@control>
2019-10-21 09:56:04 +0000

    Ajout de commentaires et modif version python

--- hf.py
+++ hf.py
@@ -1,9 +1,11 @@
+"""Import des package necessaires"""
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/hello')
 def helloIndex():
+    """retourner le message Hello friend !"""
     return 'Hello Friend!'
-
+"""executer le script et envoyer le resultat sur le port 5000"""
 app.run(host='0.0.0.0', port=5000)
