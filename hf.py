b9bdcd26025fd89ee146ac28f4ea9f346d12de95
Vagrant Default User <vagrant@control>
2019-10-21 10:17:24 +0000

    ajout main

--- hf.py
+++ hf.py
@@ -5,7 +5,10 @@ app = Flask(__name__)
 
 @app.route('/hello')
 def helloIndex():
+
     """retourner le message Hello friend !"""
     return 'Hello Friend!'
-"""executer le script et envoyer le resultat sur le port 5000"""
-app.run(host='0.0.0.0', port=5000)
+
+""" Execution du script"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
